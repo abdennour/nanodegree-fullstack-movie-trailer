b25c0ff966f2920846e4d4c3062e5883f30f20aa
abdennour <mail@abdennoor.com>
2018-02-20 05:04:52 +0300

    refactor: Separate datasource from the business logic layer

--- src/entertainment_center.py
+++ src/entertainment_center.py
@@ -1,38 +1,29 @@
+import os
+import json
 import fresh_tomatoes
 import media
 
-MOVIES_LIST = [{
-    'id': 'KYz2wyBy3kc',
-    'title': 'Toy Story',
-    'storyline': 'Woody Memorable Moments.'
-}, {
-    'id': 'eu1jekOB7sw',
-    'title': 'The Jungle',
-    'storyline': 'Disney Movies For Kids | Movies For Kids | Animation Movies For Children'
-}, {
-    'id': 'F_QW5TI5cqA',
-    'title': 'Caillou, LiVE!',
-    'storyline': ' Fun for Kids | Videos for Toddlers | Full Episodes'
-}, {
-    'id': '3b2jrx2eMP8',
-    'title': 'Disney Movies For Kids',
-    'storyline': ' Movies For Kids - Animation Movies For Children'
-}]
 
-# lambda converter of movie structure: From Dictionary structure to Movie Object
+def map_dict_to_object(movie):
+    """ converter of movie structure: From Dictionary structure to Movie Object"""
+    return media.Movie(
+        title=movie['title'],
+        poster_image_url='https://i.ytimg.com/vi/{}/mqdefault.jpg'.format(
+            movie['id']),
+        storyline=movie['storyline'],
+        trailer_youtube_url='https://youtu.be/{}'.format(movie[
+            'id'])
+    )
 
-map_dict_to_object = lambda movie: media.Movie(
-    title=movie['title'],
-    poster_image_url='https://i.ytimg.com/vi/{}/mqdefault.jpg'.format(
-        movie['id']),
-    storyline=movie['storyline'],
-    trailer_youtube_url='https://youtu.be/{}'.format(movie['id'])
-)
 
-# Convert the whole list from list of dictionary to list of Movie instances
+JSON_PATH = os.path.join(os.getcwd(), 'src', 'movies.json')
 
-MOVIES = map(map_dict_to_object, MOVIES_LIST)
+with open(JSON_PATH) as movies_file:
+    # Convert movies from string to list of dictionary
+    MOVIES_DICT = json.loads(movies_file.read())
 
-# Generate HTML and Open Web Page
+    # Convert from list of dictionary to list of Movie instances
+    MOVIES = map(map_dict_to_object, MOVIES_DICT)
 
-fresh_tomatoes.open_movies_page(MOVIES)
+    # Generate HTML and Open Web Page
+    fresh_tomatoes.open_movies_page(MOVIES)
